d35f9feabde94e45303f9f338878bd473bc43306
abelalmeida <abelalmeida@u.boisestate.edu>
2021-04-22 21:44:20 -0600

    setting up classes for Account class

--- SavingAccount.kt
+++ SavingAccount.kt
@@ -1,5 +1,6 @@
 
 class SavingAccount( number: String, customer: Customer, balance double){
+//class SavingAccount(override number: String, override customer: Customer, override balance double) Account(number, customer, balance){
     val number: String
     val customer: Customer
     val balance: double
@@ -12,7 +13,7 @@ class SavingAccount( number: String, customer: Customer, balance double){
         }
 
 
-    fun accrue(rate: double){ }
+    override fun accrue(rate: double){ }
       interest += (balance * rate)
       balance += (balance * rate)
     }
