f51c8af6faa68d84b169289c779a4af423c305bb
abelalmeida <abelalmeida@u.boisestate.edu>
2021-04-23 08:31:04 -0600

    added checking support for parent class such as Account

--- CheckingAccount.kt
+++ CheckingAccount.kt
@@ -1,8 +1,16 @@
+/*
+* @author Abel Almeida
+*
+* Checking Account class to represent a checking account
+*/
 
+// run
 //kotlinc CheckingAccount.kt -include-runtime -d CheckingAccount.jar
 //java -jar CheckingAccount.jar
-class CheckingAccount( number: String, customer: Customer, balance: Double){
-//class CheckingAccount(override number: String, override customer: Customer, override balance: Double) Account(number, customer, balance){
+
+import Account.kt
+//class CheckingAccount( number: String, customer: Customer, balance: Double){
+class CheckingAccount(override number: String, override customer: Customer, override balance: Double) Account(number, customer, balance){
     val number: String
     val customer: Customer
     val balance: Double
