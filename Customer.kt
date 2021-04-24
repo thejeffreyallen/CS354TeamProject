dcd76cf4996d1e76e8791d5bf861337cad2b7f79
abelalmeida <abelalmeida@u.boisestate.edu>
2021-04-24 11:47:48 -0600

    edited customer

--- Customer.kt
+++ Customer.kt
@@ -1,17 +1,19 @@
 //kotlinc Customer.kt -include-runtime -d Customer.jar
 //java -jar Customer.jar
 
-class Customer(name: String){
-    val name: String
 
-    init{                    // initialize the object Customer
-      this.name = name
-
-        }
-
-
-    override fun toString():String{ 
-     return name 
-    }
-
-}    
+class Customer(name: String)
+//class Customer(name: String){
+//    val name: String
+//
+//    init{                    // initialize the object Customer
+//      this.name = name
+//
+//        }
+//
+//
+//    override fun toString():String{ 
+//     return name 
+//    }
+//
+//}    
